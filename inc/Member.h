99adf959c6a74d701b515d455c1ffcf82ba3ceeb
Carter Hrabrick <chrabri2@nd.edu>
2021-04-23 01:18:29 -0400

    Made Member.h compile

--- inc/Member.h
+++ inc/Member.h
@@ -18,16 +18,16 @@ class Member : public  Player{
     // constructor
     Member() : Player(), PrizeMoney(0), rank(0), winner( false ) {}
 
-	Member(const float& prizeMoneyIn, const int& rankIn, const bool& winnerIn, const STRING& TypeIn, const STRING& MembersIn, const STRING& campIDIn, const STRING& CampNameIn, const STRING& ChallIDIn, const STRING& ChallTypeIn, const STRING& CoordinatorIn, const float& WeightAcceptedIn, const float& VolumeIn, const double& EnergyIn, const float& SoundsVolumeIn, const float& YearsIn, const float& SurfaceTempIn, const bool& PreprocessesIn) : Player(TypeIn, MembersIn, CampIDIn, CampNameIn, ChallIDIn, ChallTypeIn, CoordinatorIn, WeightAcceptedIn, VolumeIn, EnergyIn, SoundsVolumeIn, YearsIn, SurfaceTempIn, PreprocessesIn), PrizeMoney(prizeMoneyIn), rank(rankIn), winner( winnerIn ) {}
+	Member(const float& prizeMoneyIn, const int& rankIn, const bool& winnerIn, const STRING& TypeIn, const STRING& MembersIn, const STRING& CampIDIn, const STRING& CampNameIn, const STRING& ChallIDIn, const STRING& ChallTypeIn, const STRING& CoordinatorIn, const float& WeightAcceptedIn, const float& VolumeIn, const double& EnergyIn, const float& SoundsVolumeIn, const float& YearsIn, const float& SurfaceTempIn, const bool& PreprocessesIn) : Player(TypeIn, MembersIn, CampIDIn, CampNameIn, ChallIDIn, ChallTypeIn, CoordinatorIn, WeightAcceptedIn, VolumeIn, EnergyIn, SoundsVolumeIn, YearsIn, SurfaceTempIn, PreprocessesIn), PrizeMoney(prizeMoneyIn), rank(rankIn), winner( winnerIn ) {}
 
 	// destructor
-    ~Member() {}
+    ~Member(); 
 
 	float get_PrizeMoney () const{
 		return PrizeMoney;
 	}
 
-	float set_PrizeMoney ( const float& amount ) {
+	void set_PrizeMoney ( const float& amount ) {
 		PrizeMoney = amount;
 	}
 
@@ -35,7 +35,7 @@ class Member : public  Player{
 		return rank;
 	}
 
-	int set_rank ( const int& ranking ) {
+	void set_rank ( const int& ranking ) {
 		rank = ranking;
 	}
 
@@ -43,7 +43,7 @@ class Member : public  Player{
 		return winner;
 	}
 
-	int set_winner ( const bool& status ) {
+	void set_winner ( const bool& status ) {
 		winner = status;
 	}
 
@@ -59,15 +59,16 @@ class Member : public  Player{
 		output<<"Weight Accepted: " << memberIn.WeightAccepted<<ENDL;
 		output<<"Volume: " << memberIn.Volume<<ENDL;
 		output<<"Energy: " << memberIn.Energy<<ENDL;
-		output<<"Sounds Volume: " << memberIn.CoundsVolume<<ENDL;
+		output<<"Sounds Volume: " << memberIn.SoundsVolume<<ENDL;
 		output<<"Years: " << memberIn.Years<<ENDL;
 		output<<"Surface Temp: " << memberIn.SurfaceTemp<<ENDL;
 		output<<"Preprocesses: " << memberIn.Preprocesses<<ENDL;
 		output<<"PrizeMoney: " << memberIn.PrizeMoney<<ENDL;
 		output<<"Rank: " << memberIn.rank<<ENDL;
-		if( winner ) output<<"Winner!"<<ENDL<<ENDL;
+		if( memberIn.winner ) output<<"Winner!"<<ENDL<<ENDL;
 		
 		return output;
+	}
 };
 
 #endif
