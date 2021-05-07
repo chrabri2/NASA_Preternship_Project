302508d048f77427e8f63503f518180c8158c9c4
Carter Hrabrick <chrabri2@nd.edu>
2021-05-07 17:51:13 -0400

    Beefed up csvio.h

--- inc/csvio.h
+++ inc/csvio.h
@@ -10,44 +10,190 @@ class CSVReader {
 	private:
 		std::ofstream outstream;
 		std::ifstream instream;
+		std::vector<std::string> fields; // In case it is needed in future
+		
+		// Parses a delimited string (by ',') into a vector
+		std::vector<std::string> splitstring(std::string& line) const {
+			std::vector<std::string> parsed_string; // Delimited version of line
+			
+			// This block uses ',' as a delimiter to split the string into a vector
+			size_t pos = 0;
+			std::string field;
+			while ((pos = line.find(',')) != std::string::npos) { // Continues as long as ',' is found
+			    field = line.substr(0, pos); 
+				parsed_string.push_back(field); // Adds field to the vector
+				line.erase(0, pos + 1); // Removes the first part of the string
+			}
+			parsed_string.push_back(line); // Gets the last field that doesn't have a ','.
+			
+			return parsed_string;
+		}
+		
+		std::string ownertostring(const Owner& owner_in) {
+			std::string line = "";
+			line += owner_in.get_Type().append(",");
+			line += owner_in.get_Members().append(",");
+			line += owner_in.get_CampID().append(",");
+			line += owner_in.get_CampName().append(",");
+			line += owner_in.get_ChallID().append(",");
+			line += owner_in.get_ChallType().append(",");
+			line += owner_in.get_Coordinator().append(",");
+			line += std::to_string(owner_in.get_WeightAccepted()).append(",");
+			line += std::to_string(owner_in.get_Volume()).append(",");
+			line += std::to_string(owner_in.get_Energy()).append(",");
+			line += std::to_string(owner_in.get_SoundsVolume()).append(",");
+			line += std::to_string(owner_in.get_Years()).append(",");
+			line += std::to_string(owner_in.get_SurfaceTemp()).append(",");
+			line += std::to_string((int)owner_in.get_Preprocesses()).append(",");
+			line += std::to_string(owner_in.get_AwardAmount()).append(",");
+			line += std::to_string(owner_in.get_ActualCost()).append(",");
+			line += std::to_string(owner_in.get_BudgetCost()).append(",");
+			line += std::to_string(owner_in.get_EstDollars()).append(",");
+			
+			return line;
+		}
+		
+		
+		std::string membertostring(const Member& member_in) {
+			std::string line = "";
+			line += member_in.get_Type().append(",");
+			line += member_in.get_Members().append(",");
+			line += ",";
+			line += ",";
+			line += member_in.get_CampID().append(",");
+			line += member_in.get_CampName().append(",");
+			line += member_in.get_ChallID().append(",");
+			line += member_in.get_ChallType().append(",");
+			line += member_in.get_Coordinator().append(",");
+			line += std::to_string(member_in.get_WeightAccepted()).append(",");
+			line += std::to_string(member_in.get_Volume()).append(",");
+			line += std::to_string(member_in.get_Energy()).append(",");
+			line += std::to_string(member_in.get_SoundsVolume()).append(",");
+			line += std::to_string(member_in.get_Years()).append(",");
+			line += std::to_string(member_in.get_SurfaceTemp()).append(",");
+			line += std::to_string((int)member_in.get_Preprocesses()).append(",");
+			
+			return line;
+		}
+		
+		// Reads a line and sets the fields variable
+		void setfields() {
+			std::string line;
+			if (!std::getline(instream, line)) {
+				std::cout << "The input file is missing fields" << std::endl;
+			}
+			
+			outstream << line << std::endl;
+			
+			fields = splitstring(line);
+		}
 		
 	public:
-		
-		// Copy Constructor(shouldn't be used)
-		CSVReader(const CSVReader& CSVReader_in);
 		
 		// File contructor (this is the one)
 		CSVReader(const char* &file_output, const char* &file_input) :
 			outstream(file_output),
-			instream(file_input) {}
+			instream(file_input), 
+			fields() {setfields();}
 		
-		// Destructor (important for closing streams)
-		~CSVReader() {
+		// closer (important for closing streams)
+		void close() {
 			outstream.close();
 			instream.close();
 		}
 		
-		// Assignment Operator (shouldn't be used)
-		CSVReader& operator= (const CSVReader& rhs);
-		
-		void readline(/*Owner &owner_in*/) {
+		bool readline(Owner& owner_in) {
 			// Reads one line from instream to line
 			std::string line;
-			std::getline(instream, line);
-			std::vector<std::string> parsed_string; // Delimited version of line
-			
-			size_t pos = 0;
-			std::string token;
-			while ((pos = line.find(',')) != std::string::npos) {
-			    token = line.substr(0, pos);
-				parsed_string.push_back(token);
-				line.erase(0, pos + 1);
+			if (!std::getline(instream, line)) {
+				std::cout << "Readline either found an empty line or the end of file" << std::endl;
+				return false;
 			}
-			std::cout << token << std::endl;
 			
-			Owner owner_out();
+			// Parse the string to a vector
+			std::vector<std::string> parsed_string = splitstring(line);
+			
+			// Flag for constructor at 13
+			bool preprocess_waste;
+			if (parsed_string.at(13) == "y" || parsed_string.at(13) == "Y") 
+				preprocess_waste = true;
+			else
+				preprocess_waste = false;
+			
+			Owner owner_out(
+				std::stof(parsed_string.at(14)),
+				std::stof(parsed_string.at(15)),
+				std::stof(parsed_string.at(16)),
+				std::stof(parsed_string.at(17)),
+				parsed_string.at(0),
+				parsed_string.at(1),
+				parsed_string.at(2),
+				parsed_string.at(3),
+				parsed_string.at(4),
+				parsed_string.at(5),
+				parsed_string.at(6),
+				std::stof(parsed_string.at(7)),
+				std::stof(parsed_string.at(8)),
+				std::stod(parsed_string.at(9)),
+				std::stof(parsed_string.at(10)),
+				std::stof(parsed_string.at(11)),
+				std::stof(parsed_string.at(12)),
+				preprocess_waste
+			);
+			
+			owner_in = owner_out;
+			return true;
 		}
 
-		void readline(Member &member_in);
+		bool readline(Member& member_in) {
+			// Reads one line from instream to line
+			std::string line;
+			if (!std::getline(instream, line)) {
+				std::cout << "Readline either found an empty line or the end of file" << std::endl;
+				return false;
+			}
+
+			// Parse the string to a vector
+			std::vector<std::string> parsed_string = splitstring(line);
+			
+			// Flag for constructor at 13
+			bool preprocess_waste;
+			if (parsed_string.at(15) == "1") 
+				preprocess_waste = true;
+			else
+				preprocess_waste = false;
+			
+			Member member_out(
+				0,
+				0,
+				false,
+				parsed_string.at(0),
+				parsed_string.at(1),
+				parsed_string.at(4),
+				parsed_string.at(5),
+				parsed_string.at(6),
+				parsed_string.at(7),
+				parsed_string.at(8),
+				std::stof(parsed_string.at(9)),
+				std::stof(parsed_string.at(10)),
+				std::stod(parsed_string.at(11)),
+				std::stof(parsed_string.at(12)),
+				std::stof(parsed_string.at(13)),
+				std::stof(parsed_string.at(14)),
+				preprocess_waste
+			);
+			
+			member_in = member_out;
+			return true;
+		}
 		
+		void ownertocsv(const Owner& owner_in) {
+			std::string line = ownertostring(owner_in);
+			outstream << line << std::endl;
+		}
+		
+		void membertocsv(const Member& member_in) {
+			std::string line = membertostring(member_in);
+			outstream << line << std::endl;
+		}
 };
--- test/testclass.cpp
+++ test/testclass.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "../inc/Member.h"
 #include "../inc/Owner.h"
+#include "../inc/csvio.h"
 
 int main(int argc, char** argv) {	
 	if (argc > 1) {
@@ -24,8 +25,76 @@ int main(int argc, char** argv) {
 	std::cout << "| READ IN VALUES OF CLASSES                      |" << std::endl;
 	std::cout << "--------------------------------------------------" << std::endl;	
 	
-	// TODO: Add in CSV reader
+	// CSV reader/writer test
+	std::cout << "--------------------------------------------------" << std::endl;
+	std::cout << "| READ IN FIRST OWNER                            |" << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;	
+	
+	// Note: also works with Member, but you need a separate reader for each.
+	
+	// File names must be const char* type
+	const char* fileout = "vendorout.csv";
+	const char* filein = "vendor.csv";
+	
+	// The constructor for the reader, it must take BOTH an input and output
+	// file. You can make them NULL if need be
+	CSVReader reader(fileout, filein); 
+	
+	// The owner that will be written to
+	Owner test_owner;
+	
+	// This will read the next line in the csv and write it to the owner in parameter
+	// Outputs true if successful, false if not
+	reader.readline(test_owner); 
+	
+	std::cout << test_owner << std::endl;
+	
+	// Don't forget to close the reader
+	std::cout << "--------------------------------------------------" << std::endl;
+	std::cout << "| WRITE OUT FIRST OWNER                          |" << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;	
+	
+	// Takes in an owner and writes it to the csv
+	reader.ownertocsv(test_owner);
+	
+	// Don't forget to close the reader
+	reader.close();
+	
+	// This is just printing the added line in the csv
+	std::ifstream file(fileout);
+	std::string temp;
+	std::getline(file, temp);
+	std::getline(file, temp);
+	std::cout << temp << std::endl;
+	file.close();
+	
+	std::cout << "--------------------------------------------------" << std::endl;
+	std::cout << "| READ IN FIRST MEMBER                           |" << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;	
+	const char* filein2 = "solutions.csv";
+	const char* fileout2 = "solutionsout.csv";
+	
+	CSVReader reader2(fileout2, filein2);
+	Member test_member;
+	reader2.readline(test_member);
+	
+	std::cout << test_member << std::endl;
+	
+	std::cout << "--------------------------------------------------" << std::endl;
+	std::cout << "| WRITE OUT FIRST VENDOR                         |" << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;	
+	
+	reader2.membertocsv(test_member);
 
+	reader2.close();
+
+	std::ifstream file2(fileout2);
+	std::getline(file2, temp);
+	std::getline(file2, temp);
+	std::cout << temp << std::endl;
+	file2.close();
+	
+	
 	// Queue test
 	
 	// Reader test
