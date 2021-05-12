fa034bdafceafcf1d947d990b487d98bd9a93dcb
Maria Markert <mmarkert@nd.edu>
2021-05-12 10:54:36 -0400

    update

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -15,7 +15,7 @@ void draw_boxes(int x, int y){
 	
 	
 	for( int i = 1; i <= 7; i++)
-		gfx_fill_rectangle(x/4, i*y/8, x/5, y/9);
+		gfx_fill_rectangle(x/5, i*y/8, x/5, y/9);
 
 }
 
@@ -23,14 +23,14 @@ void text_options(int x, int y){
 
 	gfx_color(200, 200, 200);
 	
-	gfx_text(x/5, 8*y/9"Select option on keyboard");
-	gfx_text(x/5, 7*y/9, "1. overall");
-	gfx_text(x/5, 6*y/9, "weight");
-	gfx_text(x/5, 5*y/9, "volume");
-	gfx_text(x/5, 4*y/9, "energy");
-	gfx_text(x/5, 3*y/9, "sounds");
-	gfx_text(x/5, 2*y/9, "years");
-	gfx_text(x/5, y/9, "surface temp");
+	gfx_text(x/5, y/9"Select option on keyboard");
+	gfx_text(x/5, 2*y/9, "1. overall");
+	gfx_text(x/5, 3*y/9, "2. weight");
+	gfx_text(x/5, 4*y/9, "3. volume");
+	gfx_text(x/5, 5*y/9, "4. energy");
+	gfx_text(x/5, 6*y/9, "5. sounds");
+	gfx_text(x/5, 7*y/9, "6. years");
+	gfx_text(x/5, 8*y/9, "7. surface temp");
 
 }
 
@@ -39,7 +39,7 @@ int draw_frame( int c, int x, int y, const char *first, int first_money, const c
 	gfx_changefont("12x24");
 	
 	gfx_clear_color(10, 10, 300);
-	gfx_color(10,10,10);
+	gfx_color(100,100,100);
 
 	draw_boxes(x,y);
 	draw_button(c, x, y);
@@ -49,13 +49,13 @@ int draw_frame( int c, int x, int y, const char *first, int first_money, const c
 	char buffer[BUFSIZ];
 
 	std::sprintf(buffer, "1. %s     $%d",first, first_money);
-	gfx_text(3*x/4, 3*y/4, buffer);
+	gfx_text(3*x/4, y/4, buffer);
 
 	std::sprintf(buffer, "2. %s     $%d",second, second_money);
 	gfx_text(3*x/4, 2*y/4, buffer);
 
 	std::sprintf(buffer,"3. %s     $%d",third, third_money);
-	gfx_text(3*x/4, 1*y/4, buffer);
+	gfx_text(3*x/4, 3*y/4, buffer);
 
 	gfx_flush();
 /*
