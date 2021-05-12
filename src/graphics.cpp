676c79f867f4043c46a06180515fe3ef57bf6b15
Maria Markert <mmarkert@nd.edu>
2021-05-12 11:00:44 -0400

    update

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -14,8 +14,8 @@ void draw_button(int i, int x, int y){
 void draw_boxes(int x, int y){
 	
 	
-	for( int i = 1; i <= 7; i++)
-		gfx_fill_rectangle(x/5, i*y/8, x/5, y/9);
+	for( int i = 1; i <= 3; i++)
+		gfx_fill_rectangle(3*x/4, i*y/4, x/5, y/9);
 
 }
 
@@ -25,20 +25,20 @@ void text_options(int x, int y){
 
 	gfx_color(200, 200, 200);
 	
-	gfx_text(x/5, 10*y/9"Select option on keyboard");
-	gfx_text(x/5, 11*y/18, "1. overall");
-	gfx_text(x/5, 12*y/18, "2. weight");
-	gfx_text(x/5, 13*y/18, "3. volume");
-	gfx_text(x/5, 14*y/18, "4. energy");
-	gfx_text(x/5, 15*y/18, "5. sounds");
-	gfx_text(x/5, 16*y/18, "6. years");
-	gfx_text(x/5, 17*y/18, "7. surface temp");
+	gfx_text(15, 10*y/18, "Select option on keyboard");
+	gfx_text(15, 11*y/18, "1. overall");
+	gfx_text(15, 12*y/18, "2. weight");
+	gfx_text(15, 13*y/18, "3. volume");
+	gfx_text(15, 14*y/18, "4. energy");
+	gfx_text(15, 15*y/18, "5. sounds");
+	gfx_text(15, 16*y/18, "6. years");
+	gfx_text(15, 17*y/18, "7. surface temp");
 
 }
 
 int draw_frame( int c, int x, int y, const char *first, int first_money, const char *second, int second_money, const char * third, int third_money) {
 	
-	
+	gfx_clear();
 	
 	gfx_clear_color(10, 10, 300);
 	gfx_color(100,100,100);
@@ -47,16 +47,16 @@ int draw_frame( int c, int x, int y, const char *first, int first_money, const c
 	draw_button(c, x, y);
 	text_options(x, y);
 	
-	gfx_changefont("12x24");
+	gfx_changefont(char *"12x24");
 	char buffer[BUFSIZ];
 
-	std::sprintf(buffer, "1. %s     $%d",first, first_money);
+	std::sprintf(buffer, "1. %s     score: %d",first, first_money);
 	gfx_text(3*x/4, y/4, buffer);
 
-	std::sprintf(buffer, "2. %s     $%d",second, second_money);
+	std::sprintf(buffer, "2. %s     score: %d",second, second_money);
 	gfx_text(3*x/4, 2*y/4, buffer);
 
-	std::sprintf(buffer,"3. %s     $%d",third, third_money);
+	std::sprintf(buffer,"3. %s     score: %d",third, third_money);
 	gfx_text(3*x/4, 3*y/4, buffer);
 
 	gfx_flush();
